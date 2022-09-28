57d30f2cca02328e454eeda218eff7c1ea3fcff3
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-09-28 16:08:57 -0700

    correct value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,11 @@
 def best_score(a_dictionary):
     v = list(a_dictionary.values())
     max = 0
+    if a_dictionary is None or a_dictionary == {}:
+        return None
     for i in range(len(v)):
         if v[i] > max:
             max = v[i]
     for x, y in a_dictionary.items():
         if y == max:
             return x
-        else:
-            return None
