4a286125ffdbda9ce46597b8977340f8f5b4768c
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-10-11 17:29:14 +0100

    task2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A class that defines a square"""
+
+class Square:
+    """Represent a square"""
+
+
+    def __init__(self, size):
+        """Initializing a private attribute
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
